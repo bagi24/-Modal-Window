f91ba0d9a58e0c9c76d400a713d28a3c356bb3b9
gioin <injgia15@gmail.com>
2022-08-13 11:35:44 +0400

    new challange

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+const openModal = document.querySelectorAll('.show-modal');
+
+const openModalf = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalf = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < openModal.length; i++)
+  console.log(openModal[i].addEventListener('click', openModalf));
+
+closeModal.addEventListener('click', closeModalf);
+
+overlay.addEventListener('click', closeModalf);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModalf();
+  }
+});
